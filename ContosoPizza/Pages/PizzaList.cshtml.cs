c69b55be20e011bc409646fd5152687f442bdc31
rorikwong <108846354+rorikwong@users.noreply.github.com>
2023-05-29 08:55:50 -0500

    added some more services

--- ContosoPizza/Pages/PizzaList.cshtml.cs
+++ ContosoPizza/Pages/PizzaList.cshtml.cs
@@ -1,3 +1,5 @@
+using ContosoPizza.Models;
+using ContosoPizza.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,16 @@ namespace ContosoPizza.Pages
 {
     public class PizzaListModel : PageModel
     {
+        private readonly PizzaService _service;
+        public IList<Pizza> PizzaList {get;set; } = default!;
+
+        public PizzaListModel(PizzaService service)
+        {
+            _service = service;
+        }
         public void OnGet()
         {
+            PizzaList = _service.GetPizzas();
         }
     }
 }
